b1890992659167b0e85f7fd73480777718d9730b
Pritam Das <1996pritamdas@gmail.com>
2026-05-26 22:55:06 +0530

    fix django settings path

--- api/index.py
+++ api/index.py
@@ -2,9 +2,11 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'backend'))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathe.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "backend.breathe.settings"
+)
 
 from django.core.wsgi import get_wsgi_application
 
